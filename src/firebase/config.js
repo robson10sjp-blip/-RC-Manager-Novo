959f22968d0af05df84828a74f829948c65f1bd7
robson10sjp-blip <robson10sjp@gmail.com>
2026-07-22 03:16:34 +0000

    Firebase env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXYSMGNmCn9ZZd2RjOeJFZ2YSJfMfCjlA",
-  authDomain: "rc-manager-novo.firebaseapp.com",
-  projectId: "rc-manager-novo",
-  storageBucket: "rc-manager-novo.firebasestorage.app",
-  messagingSenderId: "395328443872",
-  appId: "1:395328443872:web:e1d95127ae9c7d7b448785",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
+export default app;
